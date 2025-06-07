3c12e8eb25be3c030913209bc24bed5743d9e5b7
Chelsa Jons <chelsajons@Chelsas-MacBook-Air-2.local>
2025-06-07 16:43:51 -0500

    initial commit

---
+++ hw1.js
@@ -0,0 +1,21 @@
+/*
+    Name: Chelsa Jons
+    Data created: June 6, 2025
+    Data last edited:
+    Version:1.0
+    Description: Homework 1 JS Patient Information Form
+*/
+
+//dynamic date js code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+
+//name slider js code
+let slider = document.getElementById("range");
+let output = document.getElementById("range-slider");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
